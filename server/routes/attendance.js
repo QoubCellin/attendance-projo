98a5f76c59f37c23235991b36627f72571553f26
kass8mal8 <kassimaly21@gmail.com>
2025-05-29 11:01:49 +0300

    fixed

--- server/routes/attendance.js
+++ server/routes/attendance.js
@@ -1,7 +1,8 @@
 const { Router } = require('express');
-const { markAttendance } = require('../controllers/attendance');
+const { markAttendance, viewAttendance } = require('../controllers/attendance');
 const router = new Router();
 
 router.post('/mark-attendance', markAttendance);
+router.get('/all', viewAttendance);
 
 module.exports = router;
